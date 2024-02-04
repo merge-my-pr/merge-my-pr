a8ee4c2524dc1077fd10bbaa8b864c72f45ef8cd
Ronak_Suthar <ronakronu02@gmail.com>
2024-02-04 22:37:52 +0530

    updated

--- updatescript.py
+++ updatescript.py
@@ -13,7 +13,7 @@ if not event_path:
 with open(event_path, 'r') as file:
     event_data = json.load(file)
 
-
+head_row, head_col=-1,-1
 def printBoard(matrix):
     markdown_content = "## HELLO THIS IS MERGE\n## Hey Lets Play :\n|   | 0 | 1 | 2 | 3 | 4 | 5 |\n| - | - | - | - | - | - | - |\n"
     for i, row in enumerate(matrix):
@@ -53,7 +53,6 @@ issue_author = event_data['issue']['user']['login']
 issue_name =event_data['issue']['title']
 
 def move_snake(matrix, direction):
-    head_row, head_col=matrix["HangryHunger"],matrix["HangryHunger"]
     for i, row in enumerate(matrix):
         for j, cell in enumerate(row):
             if cell == "HangryHunger": #[1,3]
