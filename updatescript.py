16a8640c9b65b8b093e251abcb68a70c6a29a4c6
Ronak_Suthar <ronakronu02@gmail.com>
2024-02-04 16:16:40 +0530

    init

--- updatescript.py
+++ updatescript.py
@@ -6,13 +6,16 @@ if not event_path:
     print("GitHub event data not found.")
     exit(1)
 
+
 with open(event_path, 'r') as file:
     event_data = json.load(file)
 
 
 issue_author = event_data['issue']['user']['login']
+issue_name =event_data['issue']['title']
 
-
+if issue_name=="MOVE|UP":
+        pass  
 with open('Readme.md', 'a') as file:
     file.write(f"hellow {issue_author}\n")
 
