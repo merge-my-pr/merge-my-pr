7c17a4274f0f00a94363a2036462ca23d9714590
Ronak_Suthar <ronakronu02@gmail.com>
2024-02-04 16:35:18 +0530

    initialized

--- updatescript.py
+++ updatescript.py
@@ -8,25 +8,24 @@ event_path = os.getenv('GITHUB_EVENT_PATH')
 if not event_path:
     print("GitHub event data not found.")
     exit(1)
-
 def printBoard(matrix):
-     markdown_content = "## HELLO THIS IS MERGE\n## Hey Lets Play :\n|   | 1 | 2 | 3 | 4 | 5 | 6 |\n| - | - | - | - | - | - | - |"
-     for i, row in enumerate(matrix):
+    markdown_content = "## HELLO THIS IS MERGE\n## Hey Lets Play :\n|   | 1 | 2 | 3 | 4 | 5 | 6 |\n| - | - | - | - | - | - | - |\n"
+    for i, row in enumerate(matrix):
         markdown_content += f"| {i+1} | "
         for j, cell in enumerate(row):
-             if cell == "HangryHunger":
+            if cell == "HangryHunger":
                 markdown_content += "![](https://github.com/merge-my-pr/merge-my-pr/blob/main/files/HangryHunger.png) | "
-             elif cell == "HangryHungerBody":
-                markdown_content += "![](https://github.com/merge-my-pr/merge-my-pr/blob/main/files/HangryHubgerBody.png) |"
-             elif cell == "HangryHungerTail" :
-                markdown_content += "![](https://github.com/merge-my-pr/merge-my-pr/blob/main/files/HangryHungerTail.png)"  
-             elif cell == "Tile":
-                 markdown_content += "![](https://github.com/merge-my-pr/merge-my-pr/blob/7311931b11cdacc8bb992244a5bb4aedbd8520a3/files/GrayTile.png)"
-             else:
-                 print("invalid space")
+            elif cell == "HangryHungerBody":
+                markdown_content += "![](https://github.com/merge-my-pr/merge-my-pr/blob/main/files/HangryHubgerBody.png) | "
+            elif cell == "HangryHungerTail":
+                markdown_content += "![](https://github.com/merge-my-pr/merge-my-pr/blob/main/files/HangryHungerTail.png) | "
+            elif cell == "Tile":
+                markdown_content += "![](https://github.com/merge-my-pr/merge-my-pr/blob/7311931b11cdacc8bb992244a5bb4aedbd8520a3/files/GrayTile.png) | "
+            else:
+                print("Invalid space")
         markdown_content += "\n"
+    return markdown_content
 
-     return markdown_content
 
 matrix = [
     ["Tile"] * 7,
